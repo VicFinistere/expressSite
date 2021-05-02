39ead11d182c3d46c681ea9ebd93a8880f0eddec
Zoe <zoebelleton@gmail.com>
2021-05-02 22:17:54 +0200

    index.html

--- .history/app_20210502221716.js
+++ .history/app_20210502221716.js
@@ -25,7 +25,7 @@ app.get('/db', async (req, res) => {
     const client = await pool.connect();
     const result = await client.query('SELECT * FROM test_table');
     const results = { 'results': (result) ? result.rows : null};
-    res.sendFile('index.html', results );
+    res.render('index.html', results );
     client.release();
   } catch (err) {
     console.error(err);
